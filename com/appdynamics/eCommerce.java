ef291ac87dcccffaed07ad6f89e626e3a01510ad
Michael Englert <michael.englert@osxltmengl.fritz.box>
2017-04-22 13:40:03 +0200

    Code Cleanup

--- com/appdynamics/eCommerce.java
+++ com/appdynamics/eCommerce.java
@@ -1,37 +1,31 @@
-/*    */ package com.appdynamics;
-/*    */ 
-/*    */ import java.util.concurrent.ExecutorService;
-/*    */ import java.util.concurrent.Executors;
-/*    */ import java.util.concurrent.TimeUnit;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class eCommerce
-/*    */ {
-/*    */   public static void main(String[] args)
-/*    */   {
-/* 13 */     boolean running = true;
-/*    */     
-/* 15 */     ExecutorService executor = Executors.newFixedThreadPool(100);
-/*    */     
-/* 17 */     while (running) {
-/* 18 */       eCommerceThread eCommerceThread = new eCommerceThread();
-/* 19 */       executor.execute(eCommerceThread);
-/*    */       try
-/*    */       {
-/* 22 */         TimeUnit.MILLISECONDS.sleep(500L);
-/*    */       } catch (InterruptedException e) {
-/* 24 */         e.printStackTrace();
-/*    */       }
-/*    */     }
-/*    */     
-/* 28 */     executor.shutdown();
-/* 29 */     while (!executor.isTerminated()) {}
-/*    */   }
-/*    */ }
+package com.appdynamics;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
-/* Location:              /Users/michael.englert/Downloads/home/ubuntu/SampleApplication/BiQLevel1.jar!/com/appdynamics/eCommerce.class
- * Java compiler version: 7 (51.0)
- * JD-Core Version:       0.7.1
- */
+
+public class eCommerce
+{
+  public static void main(String[] args)
+  {
+    boolean running = true;
+
+    ExecutorService executor = Executors.newFixedThreadPool(100);
+
+    while (running) {
+      eCommerceThread eCommerceThread = new eCommerceThread();
+      executor.execute(eCommerceThread);
+      try
+      {
+        TimeUnit.MILLISECONDS.sleep(500L);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+    executor.shutdown();
+    while (!executor.isTerminated()) {}
+  }
+}
--- com/appdynamics/eCommerceThread.java
+++ com/appdynamics/eCommerceThread.java
@@ -1,201 +1,185 @@
-/*     */ package com.appdynamics;
-/*     */ 
-/*     */ import java.util.Random;
-/*     */ import java.util.concurrent.TimeUnit;
-/*     */ import java.util.logging.Level;
-/*     */ import java.util.logging.Logger;
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ public class eCommerceThread
-/*     */   implements Runnable
-/*     */ {
-/*  13 */   private static final Logger log = Logger.getLogger(eCommerceThread.class.getName());
-/*     */   private String sessionID;
-/*     */   
-/*  16 */   public static int randInt(int min, int max) { Random rand = new Random();
-/*  17 */     int randomNum = rand.nextInt(max - min + 1) + min;
-/*  18 */     return randomNum;
-/*     */   }
-/*     */   
-/*     */   public String getSessionID() {
-/*  22 */     return this.sessionID;
-/*     */   }
-/*     */   
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */ 
-/*     */   public void run()
-/*     */   {
-/*  37 */     RandomCustomer randomCustomer = new RandomCustomer();
-/*  38 */     String customerType = randomCustomer.customerType;
-/*  39 */     String customerEmail = randomCustomer.customerEmail;
-/*  40 */     String productType = randomCustomer.productType;
-/*  41 */     String productCategory = randomCustomer.productCategory;
-/*  42 */     String productName = randomCustomer.productName;
-/*  43 */     Integer productPrice = randomCustomer.productPrice;
-/*     */     
-/*     */ 
-/*  46 */     this.sessionID = ("Session" + randInt(1, 1000000));
-/*     */     
-/*     */ 
-/*  49 */     boolean nextStep = homePage();
-/*     */     int price;
-/*  51 */     if (nextStep) {
-/*  52 */       nextStep = logIn();
-/*     */       
-/*  54 */       if (nextStep) {
-/*  55 */         nextStep = addToCart();
-/*     */         
-/*  57 */         if (nextStep) {
-/*  58 */           price = checkOut(customerType, customerEmail, productType, productCategory, productName, productPrice);
-/*     */         }
-/*     */       }
-/*     */     }
-/*     */   }
-/*     */   
-/*     */ 
-/*     */   public boolean homePage()
-/*     */   {
-/*  67 */     processTransaction();
-/*     */     
-/*     */ 
-/*     */ 
-/*  71 */     int rand = randInt(1, 100);
-/*  72 */     if (rand <= 95) {
-/*  73 */       normalTransaction();
-/*  74 */     } else if ((rand > 95) && (rand <= 98)) {
-/*  75 */       slowTransaction();
-/*  76 */     } else if (rand > 98) {
-/*  77 */       errorTransaction("Random Error!");
-/*     */     }
-/*     */     
-/*     */ 
-/*     */ 
-/*  82 */     rand = randInt(1, 100);
-/*  83 */     if (rand <= 85) {
-/*  84 */       return true;
-/*     */     }
-/*  86 */     return false;
-/*     */   }
-/*     */   
-/*     */ 
-/*     */   public boolean logIn()
-/*     */   {
-/*  92 */     processTransaction();
-/*     */     
-/*     */ 
-/*     */ 
-/*  96 */     int rand = randInt(1, 100);
-/*  97 */     if (rand <= 95) {
-/*  98 */       normalTransaction();
-/*  99 */     } else if ((rand > 95) && (rand <= 98)) {
-/* 100 */       slowTransaction();
-/* 101 */     } else if (rand > 98) {
-/* 102 */       errorTransaction("Random Error!");
-/*     */     }
-/*     */     
-/*     */ 
-/*     */ 
-/*     */ 
-/* 108 */     rand = randInt(1, 100);
-/* 109 */     if (rand <= 75) {
-/* 110 */       return true;
-/*     */     }
-/* 112 */     return false;
-/*     */   }
-/*     */   
-/*     */ 
-/*     */   public boolean addToCart()
-/*     */   {
-/* 118 */     processTransaction();
-/*     */     
-/*     */ 
-/*     */ 
-/* 122 */     int rand = randInt(1, 100);
-/* 123 */     if (rand <= 95) {
-/* 124 */       normalTransaction();
-/* 125 */     } else if ((rand > 95) && (rand <= 98)) {
-/* 126 */       slowTransaction();
-/* 127 */     } else if (rand > 98) {
-/* 128 */       errorTransaction("Random Error!");
-/*     */     }
-/*     */     
-/*     */ 
-/*     */ 
-/* 133 */     rand = randInt(1, 100);
-/* 134 */     if (rand <= 30) {
-/* 135 */       return true;
-/*     */     }
-/* 137 */     return false;
-/*     */   }
-/*     */   
-/*     */ 
-/*     */   public int checkOut(String customerType, String customerEmail, String productType, String productCategory, String productName, Integer itemPrice)
-/*     */   {
-/* 143 */     processTransaction();
-/*     */     
-/*     */ 
-/*     */ 
-/* 147 */     if (((customerType.equals("Platinum")) && (productName.equals("Lord of the Rings Trilogy"))) || ((customerType.equals("Gold")) && (productName.equals("Sacred Hoops")))) {
-/* 148 */       errorTransaction("Invalid SKU!");
-/*     */     }
-/*     */     else
-/*     */     {
-/* 152 */       int rand = randInt(1, 100);
-/* 153 */       if (rand <= 95) {
-/* 154 */         normalTransaction();
-/* 155 */       } else if ((rand > 95) && (rand <= 98)) {
-/* 156 */         slowTransaction();
-/* 157 */       } else if (rand > 98) {
-/* 158 */         errorTransaction("Random Error!");
-/*     */       }
-/*     */     }
-/*     */     
-/* 162 */     return itemPrice.intValue();
-/*     */   }
-/*     */   
-/*     */   public void processTransaction() {
-/*     */     try {
-/* 167 */       TimeUnit.MILLISECONDS.sleep(randInt(100, 200));
-/*     */     } catch (InterruptedException e) {
-/* 169 */       e.printStackTrace();
-/*     */     }
-/*     */   }
-/*     */   
-/*     */   public void normalTransaction() {
-/* 174 */     try { TimeUnit.MILLISECONDS.sleep(randInt(800, 1000));
-/*     */     } catch (InterruptedException e) {
-/* 176 */       e.printStackTrace();
-/*     */     }
-/*     */   }
-/*     */   
-/*     */   public void slowTransaction() {
-/*     */     try {
-/* 182 */       TimeUnit.MILLISECONDS.sleep(randInt(3000, 4000));
-/*     */     } catch (InterruptedException e) {
-/* 184 */       e.printStackTrace();
-/*     */     }
-/*     */   }
-/*     */   
-/*     */   public void errorTransaction(String errorMsg) {
-/*     */     try {
-/* 190 */       throw new RuntimeException(errorMsg);
-/*     */     } catch (RuntimeException ex) {
-/* 192 */       log.log(Level.SEVERE, ex.toString(), ex);
-/*     */     }
-/*     */   }
-/*     */ }
+package com.appdynamics;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
-/* Location:              /Users/michael.englert/Downloads/home/ubuntu/SampleApplication/BiQLevel1.jar!/com/appdynamics/eCommerceThread.class
- * Java compiler version: 7 (51.0)
- * JD-Core Version:       0.7.1
- */
+
+public class eCommerceThread
+  implements Runnable
+{
+  private static final Logger log = Logger.getLogger(eCommerceThread.class.getName());
+  private String sessionID;
+
+  public static int randInt(int min, int max) { Random rand = new Random();
+    int randomNum = rand.nextInt(max - min + 1) + min;
+    return randomNum;
+  }
+
+  public String getSessionID() {
+    return this.sessionID;
+  }
+  
+  public void run()
+  {
+    RandomCustomer randomCustomer = new RandomCustomer();
+    String customerType = randomCustomer.customerType;
+    String customerEmail = randomCustomer.customerEmail;
+    String productType = randomCustomer.productType;
+    String productCategory = randomCustomer.productCategory;
+    String productName = randomCustomer.productName;
+    Integer productPrice = randomCustomer.productPrice;
+
+
+    this.sessionID = ("Session" + randInt(1, 1000000));
+
+
+    boolean nextStep = homePage();
+    int price;
+    if (nextStep) {
+      nextStep = logIn();
+
+      if (nextStep) {
+        nextStep = addToCart();
+
+        if (nextStep) {
+          price = checkOut(customerType, customerEmail, productType, productCategory, productName, productPrice);
+        }
+      }
+    }
+  }
+
+
+  public boolean homePage()
+  {
+    processTransaction();
+
+
+
+    int rand = randInt(1, 100);
+    if (rand <= 95) {
+      normalTransaction();
+    } else if ((rand > 95) && (rand <= 98)) {
+      slowTransaction();
+    } else if (rand > 98) {
+      errorTransaction("Random Error!");
+    }
+
+
+
+    rand = randInt(1, 100);
+    if (rand <= 85) {
+      return true;
+    }
+    return false;
+  }
+
+
+  public boolean logIn()
+  {
+    processTransaction();
+
+
+
+    int rand = randInt(1, 100);
+    if (rand <= 95) {
+      normalTransaction();
+    } else if ((rand > 95) && (rand <= 98)) {
+      slowTransaction();
+    } else if (rand > 98) {
+      errorTransaction("Random Error!");
+    }
+
+
+
+
+    rand = randInt(1, 100);
+    if (rand <= 75) {
+      return true;
+    }
+    return false;
+  }
+
+
+  public boolean addToCart()
+  {
+    processTransaction();
+
+
+
+    int rand = randInt(1, 100);
+    if (rand <= 95) {
+      normalTransaction();
+    } else if ((rand > 95) && (rand <= 98)) {
+      slowTransaction();
+    } else if (rand > 98) {
+      errorTransaction("Random Error!");
+    }
+
+
+
+    rand = randInt(1, 100);
+    if (rand <= 30) {
+      return true;
+    }
+    return false;
+  }
+
+
+  public int checkOut(String customerType, String customerEmail, String productType, String productCategory, String productName, Integer itemPrice)
+  {
+    processTransaction();
+
+
+
+    if (((customerType.equals("Platinum")) && (productName.equals("Lord of the Rings Trilogy"))) || ((customerType.equals("Gold")) && (productName.equals("Sacred Hoops")))) {
+      errorTransaction("Invalid SKU!");
+    }
+    else
+    {
+      int rand = randInt(1, 100);
+      if (rand <= 95) {
+        normalTransaction();
+      } else if ((rand > 95) && (rand <= 98)) {
+        slowTransaction();
+      } else if (rand > 98) {
+        errorTransaction("Random Error!");
+      }
+    }
+
+    return itemPrice.intValue();
+  }
+
+  public void processTransaction() {
+    try {
+      TimeUnit.MILLISECONDS.sleep(randInt(100, 200));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void normalTransaction() {
+    try { TimeUnit.MILLISECONDS.sleep(randInt(800, 1000));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void slowTransaction() {
+    try {
+      TimeUnit.MILLISECONDS.sleep(randInt(3000, 4000));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void errorTransaction(String errorMsg) {
+    try {
+      throw new RuntimeException(errorMsg);
+    } catch (RuntimeException ex) {
+      log.log(Level.SEVERE, ex.toString(), ex);
+    }
+  }
+}
